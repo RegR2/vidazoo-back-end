0f3da0278f8eb28f689af793ec2b576e07a22f81
RegR2 <88548734+RegR2@users.noreply.github.com>
2022-11-19 20:53:03 +0200

    added port options + .env file

--- server.js
+++ server.js
@@ -5,10 +5,14 @@ const server = express();
 const domains = "/domains";
 const axios = require("axios");
 const fs = require("fs");
+require("dotenv").config();
 
-// these are here and not in a .env file so you know which port to listen on when testing
 const directory = "http://localhost:3000";
-const port = 5500;
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
 
 server.use(cors({ origin: directory, credentials: true }));
 
